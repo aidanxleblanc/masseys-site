0e659f8831d4bdf6752f898ff35939994068a129
Aidan <leblancaidanx@gmail.com>
2023-08-21 14:27:21 -0400

    Individual Project draft

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Projects from "./pages/Projects";
 import './App.css';
 import FeaturedProjects from './pages/FeaturedProjects';
 import IndividualProject from './pages/IndividualProject';
+import { type } from '@testing-library/user-event/dist/type';
 
 function App() { 
   return (
@@ -11,7 +12,20 @@ function App() {
       {/* <HomePage /> */}
       {/* <Projects /> */}
       {/* <FeaturedProjects /> */}
-      <IndividualProject name='380 Stuart Street'/>
+      <IndividualProject 
+        name='380 Stuart Street'
+        city='Boston'
+        state='Massachusetts'
+        year='2017'
+        type='Office'
+        arch='CBT Architects'
+        owner='Turner'
+        contr='Contractor'
+        desc='Situated in the center of Boston’s Back Bay neighborhood, 380 Stuart is an intelligently designed 
+        642,000 SF, 28-story office tower with private terraces, an indoor-outdoor roof deck, elevated 
+        amenities, integrated,  touchless building systems and enhanced health and well-being measures 
+        tailored to meet your needs. '
+          />
     </>
   );
 }
--- src/pages/IndividualProject.jsx
+++ src/pages/IndividualProject.jsx
@@ -1,18 +1,84 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
+import ProjectSquare from '../components/ProjectSquare'
 
-const IndividualProject = ({name}) => {
+const IndividualProject = ({url, desc, name, city, state, year, type, arch, owner, contr}) => {
   return (
-    <div className='justify-center'>
+    <div className='font-display text-navy text-xl justify-center'>
         <Navbar />
         <PageTitle title={name}/>
         <div className='flex justify-center'>
-            <div className='flex w-3/4'>
-                <img src="/Users/aidanleblanc/Desktop/masseys-site/src/images/380stuart.jpeg"/>
+            <div className='flex w-2/3'>
+                <img src={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/380stuart.jpeg')} />
             </div>
         </div>
-        
+        <div className='flex justify-center p-6'>
+            <h1 className='font-thin text-2xl text-center w-2/3'>{desc}</h1>
+        </div>
+        <div className='flex'>
+            <div className='w-1/2 text-right font-semi mr-1'>
+                Location: <br />
+                Year: <br />
+                Type: <br />
+                Architect: <br />
+                Owner/Developer: <br />
+                General Contractor: <br />
+            </div>
+            <div className='w-1/2 text-left ml-1'>
+                {city}, {state} <br />
+                {year} <br />
+                {type} <br />
+                {arch} <br />
+                {owner} <br />
+                {contr} <br />
+            </div> 
+        </div>
+        <div className='font-semi text-3xl text-center pt-6'>
+            <h1>More {state} Projects</h1>
+        </div>
+        <div className='flex p-6 space-x-4'>
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+            <ProjectSquare
+                imageUrl={require('/Users/aidanleblanc/Desktop/masseys-site/src/images/88seaport.jpeg')} 
+                name=''
+                loc=''
+            />
+        </div>
     </div>
   )
 }
